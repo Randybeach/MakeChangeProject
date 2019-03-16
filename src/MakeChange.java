00ca4f2fc13e4e8d81c0377148c6a946f6e9add9
Randybeach <beach.randyj@gmail.com>
2019-03-16 12:15:00 -0600

    Optimized decimal rounding

--- src/MakeChange.java
+++ src/MakeChange.java
@@ -12,10 +12,12 @@ public class MakeChange {
 
 		do {
 			double price = getPrice();
+			
 			double moneyGiven = getCustomerCash(price);
 
 			double changeNeeded = moneyGiven - price;
 
+			
 			System.out.println(
 					"Thank you for the $" + moneyGiven + ". I owe you $" + String.format("%.2f", changeNeeded));
 			if (changeNeeded == 0) {
@@ -100,89 +102,132 @@ public class MakeChange {
 	}
 
 	public static void getChange(double changeNeeded) throws Exception{
+		int totalChangeCounter = 0;
 		System.out.println("Give me one second to count your change");
 		pause();
 		pause();
-		System.out.println("I owe you");
+		int changeNeededInt = (int)((changeNeeded + .001)* 100);
+		System.out.println("I owe you...");
 
 		int count20 = 0;
-		while (changeNeeded > 20) {
-			changeNeeded -= 20;
+		while (changeNeededInt >= 2000) {
+			changeNeededInt -= 2000;
 			count20++;
+			totalChangeCounter ++;
 		}
-		if (count20 > 0)
+		if (count20 > 1) {
 			System.out.print(count20 + " twenties, ");
+		}else if(count20 > 0) {
+			System.out.print(count20 + " twenty, ");
+			
+		}
 			pause();
 			
 			
 		int count10 = 0;
-		while (changeNeeded > 10) {
-			changeNeeded -= 10;
+		while (changeNeededInt >= 1000) {
+			changeNeededInt -= 1000;
 			count10++;
+			totalChangeCounter ++;
 		}
-		if (count10 > 0)
+		if (count10 > 1) {
 			System.out.print(count10 + " tens, ");
+		}else if(count10 > 0) {
+			System.out.print(count10 + " ten, ");
+			
+		}
 			pause();
 			
 			
 		int count5 = 0;
-		while (changeNeeded > 5) {
-			changeNeeded -= 5;
+		while (changeNeededInt >= 500) {
+			changeNeededInt -= 500;
 			count5++;
+			totalChangeCounter ++;
 		}
-		if (count5 > 0)
+		if (count5 > 1) {
 			System.out.print(count5 + " fives, ");
+		}else if(count5 > 0) {
+			System.out.print(count5 + " five, ");
+			
+		}
 			pause();
 			
 			
 		int count1 = 0;
-		while (changeNeeded > 1) {
-			changeNeeded -= 1;
+		while (changeNeededInt >= 100) {
+			changeNeededInt -= 100;
 			count1++;
+			totalChangeCounter ++;
 		}
-		if (count1 > 0)
+		if (count1 > 1) {
 			System.out.print(count1 + " ones, ");
+		}else if(count1 > 0) {
+			System.out.print(count1 + " one, ");
+			
+		}
 			pause();
 			
 			
 		int count_25 = 0;
-		while (changeNeeded > .25) {
-			changeNeeded -= .25;
+		while (changeNeededInt >= 25) {
+			changeNeededInt -= 25;
 			count_25++;
+			totalChangeCounter ++;
 		}
-		if (count_25 > 0)
+		if (count_25 > 1) {
 			System.out.print(count_25 + " quarters, ");
+		}else if(count_25 > 0) {
+			System.out.print(count_25 + " quarter, ");
+			
+		}
 			pause();
 			
 			
 		int count_10 = 0;
-		while (changeNeeded > .10) {
-			changeNeeded -= .10;
+		while (changeNeededInt >= 10) {
+			changeNeededInt -= 10;
 			count_10++;
+			totalChangeCounter ++;
 		}
-		if (count_10 > 0)
+		if (count_10 > 1) {
 			System.out.print(count_10 + " dimes, ");
+		}else if(count_10 > 0) {
+			System.out.print(count_10 + " dime, ");
+			
+		}
 			pause();
 			
 			
 		int count_5 = 0;
-		while (changeNeeded > .05) {
-			changeNeeded -= .05;
+		while (changeNeededInt >= 5) {
+			changeNeededInt -= 5;
 			count_5++;
+			totalChangeCounter ++;
 		}
-		if (count_5 > 0)
+		if (count_5 > 1) {
 			System.out.print(count_5 + " nickles, ");
+		}else if(count_5 > 0) {
+			System.out.print(count_5 + " nickle, ");
+			
+		}
 			pause();
 			
-			
 		int count_1 = 0;
-		while (changeNeeded > .00) {
-			changeNeeded -= .01;
+		while (changeNeededInt > 0 && changeNeededInt != 0) {
+			changeNeededInt -= 1;
 			count_1++;
 		}
-		if (count_1 > 0) {
+		if (count_1 > 1 && totalChangeCounter > 0) {
 			System.out.print("and ");
 			System.out.println(count_1 + " pennies. ");
+		}else if(count_1 > 0 && totalChangeCounter > 0) {
+			System.out.print("and ");
+			System.out.println(count_1 + " penny. ");
+		}else if( count_1 > 1) {
+			System.out.println(count_1 + " pennies. ");
+		}else if(count_1 > 0) {
+			System.out.println(count_1 + " penny. ");
 		}
 		pause();
 
@@ -190,7 +235,7 @@ public class MakeChange {
 	
 	public static void pause()throws Exception{
 		//added a short pause for conversational realism
-		Thread.sleep(1000);
+		Thread.sleep(500);
 	}
 
 }
